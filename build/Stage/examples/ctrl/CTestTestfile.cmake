242183be239edd30a80acb43d95f873166b6b7f7
Gengchen1 <148647814+Gengchen1@users.noreply.github.com>
2024-11-13 10:33:39 +0300

    Delete build directory

--- build/Stage/examples/ctrl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zgc/mr_ws/src/Stage/examples/ctrl
-# Build directory: /home/zgc/mr_ws/build/Stage/examples/ctrl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
